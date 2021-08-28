4c5b43fda3384cd4ae601938ed1c4efaf1c6197a
Hasan UYANIK <40677383+hasanuyanik@users.noreply.github.com>
2021-08-28 08:44:18 +0300

    Bazal Metabolizma hesaplaması ve if,elseif,else kullanılmasının kodlanması

--- app.php
+++ app.php
@@ -34,3 +34,29 @@ $guess = "1470"; // Tahmin edilen değer
  * 
  * yazması gerekiyor.
  */
+
+if($gender === "female"){ // $gender değeri "female" ise if içerisindeki işlemleri yap
+    //Kadınlar için;
+    $metabolismConst = 447.593; // Metabolizma sabit değerinin değişkene atanması
+    $weightConst = 9.247; // Kilo sabit değerinin değişkene atanması
+    $heightConst = 3.098; // Boy sabit değerinin değişkene atanması
+    $ageConst = 4.330; // Yaş sabit değerinin değişkene atanması
+}elseif($gender === "male"){ // $gender değeri "male" ise elseif içerisindeki işlemleri yap
+    //Erkekler için;
+    $metabolismConst = 88.362; // Metabolizma sabit değerinin değişkene atanması
+    $weightConst = 13.397; // Kilo sabit değerinin değişkene atanması
+    $heightConst = 4.799; // Boy sabit değerinin değişkene atanması
+    $ageConst = 5.677; // Yaş sabit değerinin değişkene atanması
+}
+
+$basalMetabolism = $metabolismConst+($weightConst*$weight)+($heightConst*$height)-($ageConst*$age); // işlem sonucunu $basalMetabolism değişkenine ata
+
+echo "BMR: $basalMetabolism<br>"; // ekrana BMR: ile $basalMetabolism içindeki değeri yazıp alt satıra geç
+
+if($guess > $basalMetabolism){ // $guess'in değeri $basalMetabolism değerinden büyükse if içerisindeki işlem(leri) yap
+    echo "Tahmin değerinden düşük";
+}elseif($guess == $basalMetabolism){ // $guess'in değeri $basalMetabolism değerine eşitse elseif içerisindeki işlem(leri) yap
+    echo "Tahmin değerine eşit";
+}elseif($guess < $basalMetabolism){  // $guess'in değeri $basalMetabolism değerinden küçükse elseif içerisindeki işlem(leri) yap
+    echo "Tahmin değerinden büyük";
+}
